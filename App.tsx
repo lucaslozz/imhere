3db33352487d4d27ff35f3062d5d8703e4855962
lucaslozz <lucass.mark@gmail.com>
2023-06-06 13:04:54 -0300

    install logrocket

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
-import Home from "./src/screens/Home";
+import { useEffect } from 'react';
+import Home from './src/screens/Home';
 import { StatusBar } from 'react-native';
 
-
+import LogRocket from '@logrocket/react-native';
 
 export default function App() {
+  useEffect(() => {
+    LogRocket.init('l8ivti/test-app-jvdtj');
+  }, []);
   return (
     <>
       <StatusBar
-        barStyle={"light-content"}
+        barStyle={'light-content'}
         backgroundColor="transparent"
         translucent
       />
       <Home />
-    </>)
+    </>
+  );
 }
